2cede06e848d5f00c2deea3e2c8a22a31e1dcce4
EXELVI <exelvi@outlook.com>
2022-05-21 13:41:24 +0200

    Update + comment

--- script.js
+++ script.js
@@ -1,45 +1,37 @@
 //By EXELVI#8345
 //For ItzLucio#4917
+//Before using/copying ask to EXELVI#8345
+//I explained how it works, read comment
 
-
-function getUrlVars() {
-  var vars = {};
-  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-      vars[key] = value;
-  });
-  return vars;
-}
-
-
-var webh = getUrlVars()
+var webh = {};
+var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+  webh[key] = value;
+}); //Here we take url arguments (Ex: { token: "0432..." }), you can see it in the console
 
 function sendMsg() {
+  console.log(webh) //Send to console url arguments
 
+  if (!webh.token && !webh.id) return console.error("No webhook token and id found"); //If it don't find the token and id print an error
+  if (!webh.token) return console.error("No webhook token found"); //If it don't find the token print an error
+  if (!webh.id) return console.error("No webhook id found"); //If it don't find the id print an error
 
-  var request = new XMLHttpRequest();
-  request.open("POST", `https://discordapp.com/api/webhooks/${webh.id}/${webh.token}`);
+  var request = new XMLHttpRequest(); //Http request
+  request.open("POST", `https://discordapp.com/api/webhooks/${webh.id}/${webh.token}`);//It open a request to discord with the id and token of the webhook
 
-
-  request.setRequestHeader('Content-type', 'application/json');
+  request.setRequestHeader('Content-type', 'application/json'); //Set the header
 
   var params = {
-     content: webh.content, 
-     username: webh.username,
-      embeds: [{
-        "title": webh.etitle,
-        "description": webh.edescription,
-        "color": webh.ecolor,
-        "footer": webh.efooter
-      }]
-    
-    }
-
-  request.send(JSON.stringify(params));
-
+    content: webh.content,
+    username: webh.username,
+    embeds: [{
+      "title": webh.etitle,
+      "description": webh.edescription,
+      "color": webh.ecolor,
+      "footer": webh.efooter
+    }]
+  }//The variables for the webhook
+  
+  request.send(JSON.stringify(params)); //Send the request to discord (send the webhook)
 }
 
-sendMsg()
-
-
-    
-  
+sendMsg() //Starts the function
